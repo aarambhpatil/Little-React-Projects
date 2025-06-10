c8a90b209eb374265b39be35722ce80850c0de27
aarambhpatil <mortalfear8611@gmail.com>
2025-06-10 13:59:25 +0530

    Password Validator Project Complete

--- password-validator/src/App.jsx
+++ password-validator/src/App.jsx
@@ -1,35 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import validator from "validator";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(false);
+
+  const passwordChange = (password) => {
+    console.log(password);
+    validate(password);
+    setPassword(password);
+  };
+
+  const validate = (password) => {
+    console.log(password);
+    if (
+      validator.isStrongPassword(password, {
+        minLength: 8,
+        minLowercase: 1,
+        minUppercase: 1,
+        minNumbers: 1,
+        minSymbols: 1,
+      })
+    ) {
+      setError(true);
+    } else {
+      setError(false);
+    }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="bg-gray-300 w-screen h-screen">
+      <div className="flex w-full flex-col p-10 items-center">
+        <h1 className="">Enter a Password</h1>
+        <input
+          type="password"
+          className="bg-white w-100"
+          onChange={(e) => {
+            passwordChange(e.target.value);
+          }}
+        />
+        {error ? (
+          <span className="text-green-500 text-center">Strong Password</span>
+        ) : (
+          <span className="text-red-500 text-center">Weak Password</span>
+        )}
+        {error ? (
+          <button type="submit">Save Password</button>
+        ) : (
+          <button type="submit" className="disabled cursor-not-allowed">
+            Save Password
+          </button>
+        )}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
