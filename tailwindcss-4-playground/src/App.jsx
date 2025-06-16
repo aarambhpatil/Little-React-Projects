64b14672a6b96099fe763d70b2d0c21fbda83471
aarambhpatil <mortalfear8611@gmail.com>
2025-06-16 19:01:42 +0530

    Tailwind 4 Gradient Check

--- tailwindcss-4-playground/src/App.jsx
+++ tailwindcss-4-playground/src/App.jsx
@@ -1,20 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <div className='flex flex-row'>
-    <div className='w-100 h-100 bg-linear-90 from-violet-500 via-indigo-500 to-blue-500'>
+    <div className="flex flex-row">
+      <div className="w-100 h-100 bg-linear-90 from-violet-500 via-indigo-500 to-blue-500"></div>
+      <div className="w-100 h-100 bg-linear-90 from-blue-500 via-green-500 to-yellow-500"></div>
+      <div className="w-100 h-100 bg-linear-90 from-yellow-500 via-orange-500 to-red-500"></div>
     </div>
-    <div className='w-100 h-100 bg-linear-90 from-blue-500 via-green-500 to-yellow-500'>
-    </div>
-    <div className='w-100 h-100 bg-linear-90 from-yellow-500 via-orange-500 to-red-500'>
-    </div>
-    </div>
-  )
+  );
 }
 
-export default App
+export default App;
